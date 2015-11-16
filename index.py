737cf35eef3266a5eb98f87ebf9d8cab83e3d9ce
kitoo <tookikimie@gmail.com>
2015-11-16 23:27:24 +0900

    just change timing of getting all feeds for sidebar

--- index.py
+++ index.py
@@ -35,14 +35,12 @@ def index(db):
     entries = models.get_entries(db)
 
     # index.tplの描画
-    return template('index', feeds=feeds, title=None, entries=entries,
-                    errors=errors, request=request)
+    return template('index', feeds=feeds, title=None,
+                    entries=entries, errors=errors, request=request)
 
 
 @get('/<feed_id:int>')
 def show_entry_list(db, feed_id):
-    feeds = models.get_all_feeds(db)
-
     # Feedの検索
     feed = models.get_feed(db, feed_id)
     # Feedが存在しない(404を表示）
@@ -54,28 +52,27 @@ def show_entry_list(db, feed_id):
     entries = models.get_entries(db, feed_id)
 
     # index.tplの描画
-    return template('index', feeds=feeds, title=feed.title, entries=entries,
-                    errors=errors, request=request)
+    return template('index', feeds=models.get_all_feeds(db), title=feed.title,
+                    entries=entries, errors=errors, request=request)
 
 
 @get('/add')
 def new(db):
-    feeds = models.get_all_feeds(db)
-
     form = FeedForm()
 
     # add.tplの描画
-    return template('add', feeds=feeds, form=form, request=request)
+    return template('add', feeds=models.get_all_feeds(db),
+                    form=form, request=request)
 
 
 @post('/add')
 def create(db):
-    feeds = models.get_all_feeds(db)
     form = FeedForm(request.forms.decode())
 
     # フォームのバリデーション
     if not form.validate():
-        return template('add', feeds=feeds, form=form, request=request)
+        return template('add', feeds=models.get_all_feeds(db),
+                        form=form, request=request)
 
     # Feedの生成と格納
     feed = models.add_feed(db,
@@ -87,7 +84,8 @@ def create(db):
 
     if feed is None:
         form.url.errors.append(u"URLからフィードを取得できませんでした。")
-        return template('add', feeds=feeds, form=form, request=request)
+        return template('add', feeds=models.get_all_feeds(db),
+                        form=form, request=request)
 
     # feedを既存から検索し、重複していればエラー扱い&コミットしない
     same_url_feed = models.get_same_url_feed(db, feed.url)
@@ -97,7 +95,8 @@ def create(db):
             u"すでに同じフィードが登録されています。(「%s」)" % feed.title)
         # insertしてしまっているので、元に戻す(1行だけなので、modelsにはとりあえず入れないでおく)
         db.delete(feed)
-        return template('add', feeds=feeds, form=form, request=request)
+        return template('add', feeds=models.get_all_feeds(db),
+                        form=form, request=request)
 
     # 該当フィードの記事の一覧画面へリダイレクト(リダイレクト先で更新処理が行われる)
     redirect("./" + str(feed.id))
@@ -105,8 +104,6 @@ def create(db):
 
 @get('/<feed_id:int>/edit')
 def edit(db, feed_id):
-    feeds = models.get_all_feeds(db)
-
     # Feedの検索
     feed = models.get_feed(db, feed_id)
 
@@ -118,13 +115,12 @@ def edit(db, feed_id):
     form = FeedForm(request.POST, feed)
 
     # edit.tplを描画
-    return template('edit', feeds=feeds, feed=feed, form=form, request=request)
+    return template('edit', feeds=models.get_all_feeds(db),
+                    feed=feed, form=form, request=request)
 
 
 @post('/<feed_id:int>/edit')
 def update(db, feed_id):
-    feeds = models.get_all_feeds(db)
-
     # Feedの検索
     feed = models.get_feed(db, feed_id)
 
@@ -135,7 +131,8 @@ def update(db, feed_id):
     form = FeedForm(request.forms.decode())
 
     if not form.validate():
-        return template('edit', feeds=feeds, form=form, request=request)
+        return template('edit', feeds=models.get_all_feeds(db),
+                        form=form, request=request)
 
     # feed情報を更新
     feed.title = form.title.data
